581627de4af7dc9d513c690e06db9d9efe6dfdad
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-05-18 22:34:29 -0400

    user favs and gifs

--- src/components/profile/UserProfileScreen.jsx
+++ src/components/profile/UserProfileScreen.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import logo from "../../assets/img/avatar.svg";
+import { MangaCard } from "../main/MangaCard";
 
 export const UserProfileScreen = () => {
-    return (
-        <div>
-            This is the user profile
+  const {
+    auth: { user },
+    userDetails: { favorites },
+  } = useSelector((state) => state);
+  return (
+    <div className="user-profile__container">
+      <div className="user-profile__user-info">
+        <img
+          src={logo}
+          alt="user-avatar"
+          className="user-profile__user-avatar"
+        />
+        <div className="user-profile__user-data">
+          <h2>{user.displayName}</h2>
+          <hr />
         </div>
-    )
-}
+      </div>
+
+      <div className="user-profile__favorites-container">
+        <h2 className="">Your Favs</h2>
+        <hr />
+        <div className="user-profile__favorites">
+          {favorites.map((favorite) => (
+            <MangaCard
+              key={favorite.id}
+              id={favorite.id}
+              coverImage={favorite.coverImage.medium}
+              title={favorite.title.romaji}
+              sectionName="newManga"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
