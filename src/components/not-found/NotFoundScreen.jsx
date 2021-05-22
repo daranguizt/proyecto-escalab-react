6c643d5096cbbb977d0ac212a345d1e1813a363e
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-05-22 05:53:57 -0400

    vercel fix

--- src/components/not-found/NotFoundScreen.jsx
+++ src/components/not-found/NotFoundScreen.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import gif from '../../assets/img/404.gif';
 
 export const NotFoundScreen = () => {
     return (
-        <div>
-            404: Not Found
+        <div className="not_found">
+            <img src={gif} alt="kanna gif" />
+            <h3>It seems the page does not exist</h3>
+            <h2>Click the logo to go to the main page</h2>
         </div>
     )
 }
